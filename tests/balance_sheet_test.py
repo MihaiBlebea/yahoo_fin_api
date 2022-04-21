27135e08e076427a377cf9381e8b28496aa0430c
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-04-21 07:50:44 +0100

    Fix failing test

--- tests/balance_sheet_test.py
+++ tests/balance_sheet_test.py
@@ -4,32 +4,32 @@ from yahoo_api.balance_sheet import BalanceSheets
 
 class TestBalanceSheet(unittest.TestCase):
 
-	def __load_balance_sheet(self)-> BalanceSheets:
+	def __load_balance_sheets(self)-> BalanceSheets:
 		return BalanceSheets.from_input_file("AAPL", "./tests/AAPL.json")
 
 	def test_can_load_cashflows_from_file(self):
-		cf = self.__load_cashflows()
+		bs = self.__load_balance_sheets()
 
-		self.assertEqual("AAPL", cf.symbol)
-		self.assertEqual("2021-09-25", cf.cashflows[0].end_date)
+		self.assertEqual("AAPL", bs.symbol)
+		self.assertEqual("2021-09-25", bs.balance_sheets[0].end_date)
 
-	def test_can_load_cashflows_from_dict(self):
+	def test_can_load_balance_sheets_from_dict(self):
 		with open("./tests/AAPL.json") as file:
 			d = json.loads(file.read())
-			d = d["cashflowStatementHistory"]["cashflowStatements"]
-			cf = CashFlows.from_dict("AAPL", d)
+			d = d["balanceSheetHistory"]["balanceSheetStatements"]
+			bs = BalanceSheets.from_dict("AAPL", d)
 
-			self.assertEqual("AAPL", cf.symbol)
-			self.assertEqual("2021-09-25", cf.cashflows[0].end_date)
+			self.assertEqual("AAPL", bs.symbol)
+			self.assertEqual("2021-09-25", bs.balance_sheets[0].end_date)
 
 	def test_correct_data(self):
-		cf = self.__load_cashflows()
+		bs = self.__load_balance_sheets()
 
-		cf = cf.cashflows[0]
-		self.assertEqual(94680000000, cf.net_income)
-		self.assertEqual(-2819000000, cf.investments)
-		self.assertEqual(-14467000000, cf.dividends_paid)
-		self.assertEqual(12665000000, cf.net_borrowings)
+		bs = bs.balance_sheets[0]
+		self.assertEqual(34940000000, bs.cash)
+		self.assertEqual(27699000000, bs.short_term_investments)
+		self.assertEqual(51506000000, bs.net_receivables)
+		self.assertEqual(6580000000, bs.inventory)
 
 
 if __name__ == "__main__":
